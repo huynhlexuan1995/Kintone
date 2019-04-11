3a800a37bfed774da4102ede2c703c8b18a8e28b
huynh11a1 <huynhlx@fabbi.com.vn>
2019-04-11 16:15:33 +0700

    branch develop

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,15 +1,6 @@
 (function () {
     "use strict";
-
-    var arr = [];
-
-    function persionLate (arr) {
-        var arr = [{a:'xxx',b:'yyy'}];
-        arr.forEach( function(key, value) {
-            console.log(key + ": " + value);
-        });
-    }
-
+    
     kintone.events.on(['app.record.index.show','app.record.index.edit.submit'], function (event) {
         console.log(event.records);
         return event;
