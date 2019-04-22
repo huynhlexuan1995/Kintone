6104c98324ab79054816a410498f71eb32359c7f
huynh11a1 <huynhlx@fabbi.com.vn>
2019-04-22 17:29:30 +0700

    next kintone

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,8 +1,18 @@
-(function () {
-    "use strict";
-    
-    kintone.events.on(['app.record.index.show','app.record.index.edit.submit'], function (event) {
-        console.log(event.records);
-        return event;
+( function () {
+    "use strict" ;
+    kintone.events.on( 'app.record.index.show',function( event ){
+        if(document.getElementById('my_index_button')!== null ) {
+            return ;
+        }
+        var myIndexButton = document.createElement( 'button' );
+        myIndexButton.id='my_index_button' ;
+        myIndexButton.innerText = 'List button' ;
+
+        // Process when button is clicked
+        myIndexButton.onclick = function(){
+            window.confirm('You pressed now?');
+        };
+
+        kintone.app.getHeaderMenuSpaceElement().appendChild(myIndexButton);
     });
-})();
+}) ();
